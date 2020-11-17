58114f56ba0c44d88d49167029917e47465abe00
Khristina <christina.marynovych@gmail.com>
2020-11-17 20:25:15 +0200

    input recomendations

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 alert('Attention! The program deletes the fractional part if you enter a decimal fraction!');
 
 let startNumber = parseInt(prompt('input start number'));
-let endNumber = parseInt(prompt('input end number'));
-
 while (!startNumber) {
    startNumber = parseInt(prompt('You need to input THE START NUMBER that is bigger than 0'));
 } 
 
+
+let endNumber = parseInt(prompt('input end number'));
 while (!endNumber || endNumber < startNumber) {
     endNumber = parseInt(prompt('You need to input THE END NUMBER that is bigger than 0 and bigger than start number'));
 }
+
 
 let condition = confirm('Do you want to calculate without even numbers?');
 let sum = 0
